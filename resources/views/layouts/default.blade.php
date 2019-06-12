e505c7629c40d0f6c9f8aa801d86602a3d936d15
e <zenghongqiang44@163.com>
2019-06-12 16:29:37 +0800

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title','Weibo App') - Laravel 入门教程</title>
-  <link rel="stylesheet"  href="{{ mix('css/app.css')}}">
-</head>
-<body>
- @include('layouts._header')
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
 
+  <body>
+    @include('layouts._header')
 
-   <div class="container">
-    <div class="offset-md-1 col-md-10">
-     @include('shared._messages')
-     @yield('content')
-     @include('layouts._footer')
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
-   </div>
- </body>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+  </body>
 </html>
