9c0a955b793c971b2ab10cb603a499705f15e3fe
e <zenghongqiang44@163.com>
2019-06-27 02:48:38 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\MOdels;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    protected $fillable = ['content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
