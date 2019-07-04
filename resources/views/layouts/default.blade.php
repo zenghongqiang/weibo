c1bbe1910f45c9d7b4d8a39c23f2731276a808fb
e <zenghongqiang44@163.com>
2019-07-04 10:46:21 +0800

    m

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Weibo App') - </title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
 
